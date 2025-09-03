6edb7c37a34378dc50d9145f7c6bd809b9813764
Viktor - 42 <vpoka@student.42vienna.com>
2025-09-03 18:58:28 +0200

    refactor(vbc): update error message for unexpected end of input
    Improve clarity in error handling by changing "end of file" to "end of input" in the unexpected function.

--- 04/vbc/res2/given.c
+++ 04/vbc/res2/given.c
@@ -26,7 +26,7 @@ void    unexpected(char c)
     if (c)
         printf("Unexpected token '%c'\n", c);
     else
-        printf("Unexpected end of file\n");
+        printf("Unexpected end of input\n");
 }
 
 int accept(char **s, char c)
