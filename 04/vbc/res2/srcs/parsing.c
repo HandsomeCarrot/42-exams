dc8c24025c63c68d664785ed117b1b346037701f
Viktor - 42 <vpoka@student.42vienna.com>
2025-09-03 20:44:46 +0200

    refactor(parser): remove debug mode support
    Remove conditional debug logging statements and debug target from Makefile to clean up code for production use. This reverts debug additions from previous commits, simplifying the parser implementation.

--- 04/vbc/res2/srcs/parsing.c
+++ 04/vbc/res2/srcs/parsing.c
@@ -4,35 +4,25 @@ static node	*parse_add(char **s);
 
 static node	*parse_num(char **s)
 {
-	static int depth = 0;
-	node	*ret;
-	int		num;
+	static int	depth = 0;
+	node		*ret;
+	int			num;
 
 	if (isdigit(**s))
 	{
 		num = (**s) - '0';
-		if (DEBUG == 1)
-			printf("got this number: %d\n", num);
 		ret = new_node((node){VAL, num, NULL, NULL});
 		(*s)++;
 		return (ret);
 	}
 	else if (accept(s, '('))
 	{
-		if (DEBUG == 1)
-			printf("went DEEPER. Now at: %d\n", ++depth);
 		ret = parse_add(s);
 		if (ret == NULL)
 			return (NULL);
 		if (accept(s, ')'))
-		{
-			if (DEBUG == 1)
-				printf("went BACK. Now at: %d\n", --depth);
 			return (ret);
-		}
 	}
-	if (DEBUG == 1)
-		printf("failed at parse_num\n");
 	if (!**s)
 		unexpected(0);
 	return (NULL);
@@ -48,8 +38,6 @@ static node	*parse_multi(char **s)
 		return (NULL);
 	while (accept(s, '*'))
 	{
-		if (DEBUG == 1)
-			printf("got multiplication\n");
 		tmp = parse_num(s);
 		if (!tmp)
 			return (destroy_tree(ret), NULL);
@@ -68,8 +56,6 @@ static node	*parse_add(char **s)
 		return (NULL);
 	while (accept(s, '+'))
 	{
-		if (DEBUG == 1)
-			printf("got addition\n");
 		tmp = parse_multi(s);
 		if (!tmp)
 			return (destroy_tree(ret), NULL);
@@ -84,10 +70,6 @@ node	*parse(char **s)
 
 	ret = parse_add(s);
 	if (!expect(s, '\0'))
-	{
-		if (DEBUG == 1)
-			printf("failed at parse\n");
 		return (destroy_tree(ret), NULL);
-	}
 	return (ret);
 }
