163bf11c822d69e9303dd76ed858feb211d09689
HandsomeCarrot <v.poka@proton.me>
2026-09-21 01:08:45 +0200

    chore(mini_serv): subject formating

--- 06/subjects/mini_serv/main.c
+++ 06/subjects/mini_serv/main.c
@@ -1,11 +1,11 @@
 #include <errno.h>
-	#include <string.h>
-	#include <unistd.h>
-	#include <stdlib.h>
-	#include <stdio.h>
-	#include <netdb.h>
-	#include <sys/socket.h>
-	#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 int extract_message(char **buf, char **msg)
 {
@@ -57,36 +57,36 @@ char *str_join(char *buf, char *add)
 
 int main() {
 	int sockfd, connfd, len;
-	struct sockaddr_in servaddr, cli; 
+	struct sockaddr_in servaddr, cli;
 
-	// socket create and verification 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-	if (sockfd == -1) { 
-		printf("socket creation failed...\n"); 
-		exit(0); 
-	} 
+	// socket create and verification
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd == -1) {
+		printf("socket creation failed...\n");
+		exit(0);
+	}
 	else
-		printf("Socket successfully created..\n"); 
-	bzero(&servaddr, sizeof(servaddr)); 
+		printf("Socket successfully created..\n");
+	bzero(&servaddr, sizeof(servaddr));
 
-	// assign IP, PORT 
-	servaddr.sin_family = AF_INET; 
+	// assign IP, PORT
+	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(2130706433); //127.0.0.1
-	servaddr.sin_port = htons(8081); 
+	servaddr.sin_port = htons(8081);
 
-	// Binding newly created socket to given IP and verification 
-	if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0) { 
-		printf("socket bind failed...\n"); 
-		exit(0); 
-	} 
+	// Binding newly created socket to given IP and verification
+	if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0) {
+		printf("socket bind failed...\n");
+		exit(0);
+	}
 	else
 		printf("Socket successfully binded..\n");
 	len = sizeof(cli);
 	connfd = accept(sockfd, (struct sockaddr *)&cli, &len);
-	if (connfd < 0) { 
-        printf("server acccept failed...\n"); 
-        exit(0); 
-    } 
+	if (connfd < 0) {
+        printf("server acccept failed...\n");
+        exit(0);
+    }
     else
         printf("server acccept the client...\n");
 }
